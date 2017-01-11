a84001e1d307dcfa672368faba43bcf5999277fc
Daryl Jewkes <github@daryljewkes.com>
2017-01-11 11:53:02 +0000

    added chartjs

--- javascripts/mongodb.js
+++ javascripts/mongodb.js
@@ -14,8 +14,9 @@ function httpGet(theUrl){
 		if (xmlHttp.readyState == 4) {
 			if (xmlHttp.status == 200) {
 				var doc = JSON.parse(xmlHttp.responseText)[0];
-				console.log(doc)
-				buildStatPanel(doc.topUsers, doc.totalUsers, doc.topWords, doc.quickest, doc.quickestObj, doc.wins, doc.loses, doc.failed, doc.totalGames, doc.avgGameHr, doc.startTime, doc.lastGame)
+				// console.log(doc)
+				buildStatPanel(doc.topUsers, doc.totalUsers, doc.topWords, doc.quickest, doc.quickestObj, doc.wins, doc.loses, doc.failed, doc.totalGames, doc.avgGameHr, doc.startTime, doc.lastGame);
+				buildCharts(doc.categories);
 			}
 		}
 	}
@@ -23,12 +24,12 @@ function httpGet(theUrl){
 
 function buildStatPanel(topUsers, userCount, topWords, fastest, fastestObj, wins, loses, failed, total, gPerHr, timeFrom, lastGame) {
 	if (lastGame.win) {
-		document.getElementById("20q_lastGame").innerHTML = "<strong>Alexa just won!</strong> She guessed " + lastGame.word + " in " + lastGame.num + " questions :)"
+		document.getElementById("20q_lastGame").innerHTML = "<strong>Alexa just won!</strong><br>She guessed " + lastGame.word + " in " + lastGame.num + " questions :)"
 	} else {
 		if (lastGame.num >= 30) {
-			document.getElementById("20q_lastGame").innerHTML = "<strong>Alexa just lost!</strong> She couldn't guess the object after 30 questions :(";
+			document.getElementById("20q_lastGame").innerHTML = "<strong>Alexa just lost!</strong><br>She couldn't guess the object after 30 questions :(";
 		} else {
-			document.getElementById("20q_lastGame").innerHTML = "<strong>Alexa just lost</strong>, but she guessed " + lastGame.word + " in " + lastGame.num + " questions :|"
+			document.getElementById("20q_lastGame").innerHTML = "<strong>Alexa just lost</strong><br>But she guessed " + lastGame.word + " in " + lastGame.num + " questions :|"
 		}
 	}
 
@@ -53,6 +54,62 @@ function buildStatPanel(topUsers, userCount, topWords, fastest, fastestObj, wins
 		document.getElementById("20q_word" + x).innerHTML = x +". " + topWords[i].key;
 		document.getElementById("20q_word" + x + "_count").innerHTML = numberWithCommas(topWords[i].count);
 	}
+}
+
+function buildCharts(categories) {
+	// console.log(categories);
+
+	var ctx = document.getElementById("chtCats");
+	var ctx = document.getElementById("chtCats").getContext("2d");
+	var ctx = $("#chtCats");
+	var ctx = "chtCats";
+	var myChart = new Chart(ctx, {
+	    type: 'bar',
+	    data: {
+	        labels: ["Animal", "Vegetable", "Mineral", "Other", "Unknown"],
+	        datasets: [{
+	            label: 'Categories',
+	            data: [
+	            	getCountByKey('Animal', categories), 
+	            	getCountByKey('Vegetable', categories), 
+	            	getCountByKey('Mineral', categories), 
+	            	getCountByKey('Other', categories), 
+	            	getCountByKey('Unknown', categories)],
+	            backgroundColor: [
+	                'rgba(255, 99, 132, 0.2)',
+	                'rgba(54, 162, 235, 0.2)',
+	                'rgba(255, 206, 86, 0.2)',
+	                'rgba(75, 192, 192, 0.2)',
+	                'rgba(153, 102, 255, 0.2)'
+	            ],
+	            borderColor: [
+	                'rgba(255,99,132,1)',
+	                'rgba(54, 162, 235, 1)',
+	                'rgba(255, 206, 86, 1)',
+	                'rgba(75, 192, 192, 1)',
+	                'rgba(153, 102, 255, 1)'
+	            ],
+	            borderWidth: 1
+	        }]
+	    },
+	    options: {
+	    	legend: {
+	    		display: false
+	    	},
+	        scales: {
+	            yAxes: [{
+	            	display: false
+	            }]
+	        }
+	    }
+	});
+}
+
+function getCountByKey(key, arr) {
+	for(var i = 0, len = arr.length; i < len; i++) {
+		if( arr[ i ].key === key ) return arr[ i ].count;
+	}
+	return 0;
 }
 
 function numberWithCommas(x) {
