eb0ff621c825a55db2560b09d87a1aa9a9815674
Daryl Jewkes <github@daryljewkes.com>
2017-05-27 00:14:36 +0100

    added popcorn quiz

--- javascripts/app.js
+++ javascripts/app.js
@@ -13,3 +13,59 @@ function getCountByKey(key, arr) {
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
+function httpGetGameCount(theUrl, prefix){
+	var xmlHttp = null;
+	xmlHttp = new XMLHttpRequest();
+	xmlHttp.open("GET", theUrl, true);
+	xmlHttp.onreadystatechange = handleReadyStateChange;
+	xmlHttp.send(null);
+
+	function handleReadyStateChange() {
+		if (xmlHttp.readyState == 4) {
+			if (xmlHttp.status == 200) {
+				var doc = JSON.parse(xmlHttp.responseText);
+				// console.log(doc)
+				document.getElementById(prefix + '_total_players').innerHTML = numberWithCommas(doc.length);
+
+				var count = 0;
+				for (var i = 0; i < doc.length; i++) {
+					count += doc[i].games;
+				}
+
+				document.getElementById(prefix + '_total_games').innerHTML = numberWithCommas(count);
+			}
+		}
+	}
+}
+
+function httpGetStats(theUrl, prefix){
+	var xmlHttp = null;
+	xmlHttp = new XMLHttpRequest();
+	xmlHttp.open("GET", theUrl, true);
+	xmlHttp.onreadystatechange = handleReadyStateChange;
+	xmlHttp.send(null);
+
+	function handleReadyStateChange() {
+		if (xmlHttp.readyState == 4) {
+			if (xmlHttp.status == 200) {
+				// console.log(xmlHttp.responseText)
+				var doc = JSON.parse(xmlHttp.responseText);
+				// console.log(doc)
+				buildTopTen(doc.splice(0, 10), prefix);
+			}
+		}
+	}
+}
+
+function buildTopTen(topTen, prefix) {
+	// console.log(topTen, prefix)
+
+	for (var i = 0; i < topTen.length; i++) {
+		var x = i + 1;
+		var id = prefix + "_" + x;
+		if (topTen[i].userId == myId) console.log(x);
+		document.getElementById(id + "_score").innerHTML = numberWithCommas(topTen[i].score);
+		document.getElementById(id + "_games").innerHTML = numberWithCommas(topTen[i].games);
+	}
+}
--- javascripts/popcorn.js
+++ javascripts/popcorn.js
@@ -1,25 +1,25 @@
 "use strict";
 
-var trifleCountUrl = 'https://api.mongolab.com/api/1/databases/trifle/collections/game?f={"games":1,"_id":0}&apiKey=qbjPCckU4aqtUj_i5wyxpwEizWa5Ccp9';
-var trifleUrl = 'https://api.mongolab.com/api/1/databases/trifle/collections/game?s={"score":-1,"timestamp":1}&apiKey=qbjPCckU4aqtUj_i5wyxpwEizWa5Ccp9';
-var trifleAmazonUkUrl = "https://api.mongolab.com/api/1/databases/twentyquestions/collections/amazon?q={}&apiKey=qbjPCckU4aqtUj_i5wyxpwEizWa5Ccp9";
+var popcornCountUrl = 'https://api.mongolab.com/api/1/databases/popcorn/collections/game?f={"games":1,"_id":0}&apiKey=qbjPCckU4aqtUj_i5wyxpwEizWa5Ccp9';
+var popcornUrl = 'https://api.mongolab.com/api/1/databases/popcorn/collections/game?s={"score":-1,"timestamp":1}&apiKey=qbjPCckU4aqtUj_i5wyxpwEizWa5Ccp9';
+var popcornAmazonUkUrl = "https://api.mongolab.com/api/1/databases/popcorn/collections/amazon?q={}&apiKey=qbjPCckU4aqtUj_i5wyxpwEizWa5Ccp9";
 
 var myId = "amzn1.ask.account.AFJST3SHUEGBJ6AIWIBJHTJHHCU6NOUCAYBPAASUEPR6CAOPWB2MNH4T5JWE5GHTBVHRXXJFDV7TGAA4LLYETT3WIGRGKS2LEU5MR6X3HTEJW467IU7XNGI3OTBHYKZB4TMD5QYVTQ423HKYVT4J74TSCWHRGPYPJEWHFIGNJRKBOOKX4C7LSUX47V7CVIN5Q3KADVTT63FHE2A";
 
-httpGetStats(trifleUrl, 'tr');
-httpGetGameCount(trifleCountUrl, 'tr');
+httpGetStats(popcornUrl, 'pc');
+httpGetGameCount(popcornCountUrl, 'pc');
 buildAmazonParts({
 	uk: {
 		reviews: 'Awaiting publish',
 		score: 'NA'
 	}
-}, 'trifle_amazon_score');
+}, 'popcorn_amazon_score');
 
 setInterval(function () {
 	// console.log('getting');
-	// httpGetAmazon(trifleAmazonUkUrl);
-	httpGetStats(trifleUrl, 'tr');
-	httpGetGameCount(trifleCountUrl, 'tr');
+	// httpGetAmazon(popcornAmazonUkUrl);
+	httpGetStats(popcornUrl, 'pc');
+	httpGetGameCount(popcornCountUrl, 'pc');
 	// console.log('got');
 }, 60000);
 
@@ -35,7 +35,7 @@ function httpGetAmazon(theUrl){
 			if (xmlHttp.status == 200) {
 				var doc = JSON.parse(xmlHttp.responseText)[0];
 				// console.log(doc)
-				buildAmazonParts(doc, 'trifle_amazon_score');
+				buildAmazonParts(doc, 'popcorn_amazon_score');
 			}
 		}
 	}
