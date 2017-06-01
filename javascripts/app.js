14591638f8d42594681ae7c13c64275332c9f497
Daryl Jewkes <github@daryljewkes.com>
2017-06-01 07:50:50 +0100

    now supports larger top 10

--- javascripts/app.js
+++ javascripts/app.js
@@ -62,19 +62,33 @@ function httpGetStats(theUrl, prefix){
 }
 
 function buildTopTen(topTen, prefix) {
-	console.log(topTen, prefix)
+	// console.log(topTen, prefix)
+
+	var container = document.getElementById(prefix + '_scores')
+	// if (document.getElementById(container)) document.getElementById(container).innerHTML = x + star
 
 	for (var i = 0; i < topTen.length; i++) {
 		var x = i + 1;
 		var id = prefix + "_" + x;
 		var star = "";
 		if (topTen[i].userId == me) star = "*";
-		if (document.getElementById(id)) document.getElementById(id).innerHTML = x + star
-		document.getElementById(id + "_score").innerHTML = numberWithCommas(topTen[i].score);
-		document.getElementById(id + "_games").innerHTML = numberWithCommas(topTen[i].games);
-		if (topTen[i].locale) {
-			document.getElementById(id + "_locale").innerHTML =  "<img class='locale' src='./images/" + topTen[i].locale + ".png' />";
+
+		if (!document.getElementById(prefix + '_' + x)) {
+			var row = container.insertRow(-1);
+			row.id = prefix + '_' + x;
+			var cell1 = row.insertCell(0);
+			var cell2 = row.insertCell(1);
+			var cell3 = row.insertCell(2);
+			var cell4 = row.insertCell(3);			
 		}
+
+		cell1.innerHTML = x + star;
+		cell2.innerHTML = topTen[i].score;
+		cell3.innerHTML = topTen[i].games;
+		if (topTen[i].locale) {
+			cell4.innerHTML =  "<img class='locale' src='./images/" + topTen[i].locale + ".png' />";
+		}
+
 	}
 }
 
