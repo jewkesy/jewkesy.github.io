49b10c3c8fba1fa300366720e3fa84145ffb86c7
Daryl Jewkes <github@daryljewkes.com>
2017-07-14 20:49:13 +0100

    new chart for new users

--- javascripts/popcorn.js
+++ javascripts/popcorn.js
@@ -1,18 +1,23 @@
 "use strict";
 
-var popcornCountUrl = 'https://api.mongolab.com/api/1/databases/popcorn/collections/game?l=0&f={"games":1,"_id":0}&apiKey=qbjPCckU4aqtUj_i5wyxpwEizWa5Ccp9';
+var popcornCountUrl = 'https://api.mongolab.com/api/1/databases/popcorn/collections/game?l=0&f={"games":1,"startTimestamp":1,"_id":0}&apiKey=qbjPCckU4aqtUj_i5wyxpwEizWa5Ccp9';
 var popcornUrl = 'https://api.mongolab.com/api/1/databases/popcorn/collections/game?l=0&s={"score":-1,"timestamp":1}&apiKey=qbjPCckU4aqtUj_i5wyxpwEizWa5Ccp9';
 var popcornAmazonUkUrl = "https://api.mongolab.com/api/1/databases/popcorn/collections/amazon?q={}&apiKey=qbjPCckU4aqtUj_i5wyxpwEizWa5Ccp9";
 
 // var ctx = document.getElementById("pc_cht_locale").getContext('2d');
-var myDoughnutChart = new Chart(document.getElementById("pc_cht_locale").getContext('2d'), {
+var localChart = new Chart(document.getElementById("pc_cht_locale").getContext('2d'), {
     type: 'doughnut'
 });
+var newUsersChart = new Chart(document.getElementById("pc_cht_new_users").getContext('2d'), {
+    type: 'line'
+});
 
+var startDate = new Date(2017, 4, 28);
 
 httpGetStats(popcornUrl, 'pc', function (err, data) {
 	if (!err) {
-		updateCharts(myDoughnutChart, data);
+		updateCharts(data);
+		chtNewUsers(newUsersChart, data)
 	}
 });
 
@@ -28,7 +33,7 @@ setInterval(function () {
 	// console.log('getting');
 	// httpGetAmazon(popcornAmazonUkUrl);
 	httpGetStats(popcornUrl, 'pc', function (err, data) {
-		if (!err) updateCharts(myDoughnutChart, data);
+		if (!err) updateCharts(data);
 	});
 	httpGetGameCount(popcornCountUrl, 'pc');
 
@@ -101,12 +106,63 @@ function chtLocale(chart, data, options) {
 	chart.options.animation.duration = 1;
 
 	chart.update();
-
-	
 }
 
-function updateCharts(chart, data) {
-	// console.log(chart, data.length);
-	chtLocale(chart, data);
+function daydiff(first, second) {
+    return Math.round((second-first)/(1000*60*60*24));
 }
 
+function chtNewUsers(chart, data) {
+	if (!data) return;
+	// console.log(chart)
+		// console.log(data);
+	// get days from launch as x axis
+	// console.log(startDate)
+	var today = new Date();
+	var diff = daydiff(startDate, today);
+
+	var l = new Array(diff).fill("");
+	l[0] = "Launch";
+	l[diff-1] = "Today";
+	var ar = new Array(diff).fill(0);
+	// ar[3] = 40
+	// ar[13] = 40
+	// ar[43] = 40
+
+
+	for (var i = 0; i < data.length; i++) {
+		var x = data[i];
+		// console.log(x.startTimestamp)
+		var d = new Date(x.startTimestamp);
+		var df = daydiff(startDate, d)
+		// console.log(df)
+		ar[df] = ar[df]+1;
+	}
+
+	// console.log(ar)
+
+	var d = {
+		"labels": l,
+		"datasets":[{
+			"label":"New Users",
+			"data": ar,
+			"fill":false,
+			"borderColor":"rgb(75, 192, 192)",
+			"lineTension":0.1
+		}],
+		options: {}
+				
+	}
+
+	chart.data = d;
+	chart.options.animation.duration = 1;
+
+	chart.update();
+}
+
+function updateCharts(data) {
+	if (!data) return;
+	// console.log(data);
+	chtLocale(localChart, data);
+	chtNewUsers(newUsersChart, data)
+}
