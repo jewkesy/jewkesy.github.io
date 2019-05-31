f11970465a895e27a4373df3cc884616f6ba8daa
Daryl Jewkes <git@daryljewkes.com>
2019-05-31 13:15:29 +0100

    tweaks

--- javascript/reviews.js
+++ javascript/reviews.js
@@ -44,6 +44,7 @@ function buildAmazonReview(data) {
 	if (!data.jp) data.jp = {score:0,reviews:0};
 	if (!data.au) data.au = {score:0,reviews:0};
 	if (!data.fr) data.fr = {score:0,reviews:0};
+	if (!data.fr_ca) data.fr_ca = {score:0,reviews:0};
 	if (!data.es) data.es = {score:0,reviews:0};
 	if (!data.it) data.it = {score:0,reviews:0};
 	if (!data.mx) data.mx = {score:0,reviews:0};
@@ -51,7 +52,7 @@ function buildAmazonReview(data) {
 	if (!data.es_us) data.es_us = {score:0,reviews:0};
 	if (!data.ga) data.ga = {score:0,reviews:0};
 
-	var arIds = ['pc_uk_stars', 'pc_us_stars', 'pc_de_stars', 'pc_in_stars', 'pc_ca_stars', 'pc_jp_stars', 'pc_au_stars', 'pc_fr_stars', 'pc_es_stars', 'pc_it_stars', 'pc_mx_stars', 'pc_br_stars', 'pc_es_us_stars', 'pc_ga_stars'];
+	var arIds = ['pc_uk_stars', 'pc_us_stars', 'pc_de_stars', 'pc_in_stars', 'pc_ca_stars', 'pc_jp_stars', 'pc_au_stars', 'pc_fr_stars', 'pc_fr_ca_stars', 'pc_es_stars', 'pc_it_stars', 'pc_mx_stars', 'pc_br_stars', 'pc_es_us_stars', 'pc_ga_stars'];
 	var arClasses = ['a-star-0', 'a-star-0-5', 'a-star-1', 'a-star-1-5', 'a-star-2', 'a-star-2-5', 'a-star-3', 'a-star-3-5', 'a-star-4', 'a-star-4-5', 'a-star-5'];
 
 	for (var i = 0; i < arIds.length; i++) {
@@ -69,12 +70,13 @@ function buildAmazonReview(data) {
 		else if (i == 5) e.classList.add(getCssStar(data.jp.score));
 		else if (i == 6) e.classList.add(getCssStar(data.au.score));
 		else if (i == 7) e.classList.add(getCssStar(data.fr.score));
-		else if (i == 8) e.classList.add(getCssStar(data.es.score));
-		else if (i == 9) e.classList.add(getCssStar(data.it.score));
-		else if (i ==10) e.classList.add(getCssStar(data.mx.score));
-		else if (i ==11) e.classList.add(getCssStar(data.br.score));
-		else if (i ==12) e.classList.add(getCssStar(data.es_us.score));
-		else if (i ==13) e.classList.add(getCssStar(Math.ceil(data.ga.score)));
+		else if (i == 8) e.classList.add(getCssStar(data.fr_ca.score));
+		else if (i == 9) e.classList.add(getCssStar(data.es.score));
+		else if (i ==10) e.classList.add(getCssStar(data.it.score));
+		else if (i ==11) e.classList.add(getCssStar(data.mx.score));
+		else if (i ==12) e.classList.add(getCssStar(data.br.score));
+		else if (i ==13) e.classList.add(getCssStar(data.es_us.score));
+		else if (i ==14) e.classList.add(getCssStar(Math.ceil(data.ga.score)));
 	}
 
 	fadeyStuff('pc_uk_reviews', numberWithCommas(data.uk.reviews));
@@ -85,6 +87,7 @@ function buildAmazonReview(data) {
 	fadeyStuff('pc_jp_reviews', numberWithCommas(data.jp.reviews));
 	fadeyStuff('pc_au_reviews', numberWithCommas(data.au.reviews));
 	fadeyStuff('pc_fr_reviews', numberWithCommas(data.fr.reviews));
+	fadeyStuff('pc_fr_ca_reviews', numberWithCommas(data.fr_ca.reviews));
 	fadeyStuff('pc_es_reviews', numberWithCommas(data.es.reviews));
 	fadeyStuff('pc_it_reviews', numberWithCommas(data.it.reviews));
 	fadeyStuff('pc_mx_reviews', numberWithCommas(data.mx.reviews));
