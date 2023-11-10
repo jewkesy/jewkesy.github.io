6a8fc20fa4d901b29606bc52eb7ca3f39c72077a
Daryl Jewkes <git@daryljewkes.com>
2023-11-10 22:06:27 +0000

    completed lesson 3

--- citydrive/js/graphEditor.js
+++ citydrive/js/graphEditor.js
@@ -1,6 +1,7 @@
 class GraphEditor {
-	constructor(canvas, graph) {
-		this.canvas = canvas;
+	constructor(viewport, graph) {
+		this.viewport = viewport;
+		this.canvas = viewport.canvas;
 		this.graph = graph;
 
 		this.ctx = this.canvas.getContext("2d");
@@ -42,8 +43,8 @@ class GraphEditor {
 	}
 
 	#handleMouseMove(evt) {
-		this.mouse = new Point(evt.offsetX, evt.offsetY);
-		this.hovered = getNearestPoint(this.mouse, this.graph.points, 15);
+		this.mouse = this.viewport.getMouse(evt, true);
+		this.hovered = getNearestPoint(this.mouse, this.graph.points, 15 * this.viewport.zoom);
 		if (this.dragging == true) {
 			this.selected.x = this.mouse.x;
 			this.selected.y = this.mouse.y;
@@ -62,6 +63,12 @@ class GraphEditor {
 		this.hovered = null;
 		if (this.selected == point)
 			this.selected = null;
+	}
+
+	dispose() {
+		this.graph.dispose();
+		this.selected = null;
+		this.hovered = null;
 	}
 
 	display() {
